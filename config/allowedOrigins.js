0cdd8166f9cb57aade9e64d47719b82d961ca8dc
Aditi Patel <ampaditipatel@gmail.com>
2024-04-03 11:36:23 +0530

    add customer site url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'https://nexus-for-seller.netlify.app',
-    'http://127.0.0.1:5500',
+    'https://nexushere.netlify.app',
     'http://localhost:8000',
     'http://localhost:3000',
 ];
